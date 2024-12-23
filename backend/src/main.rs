cf0d500cb1397be5c6532cdc5d7ec5a0991135e0
hayatroid <hayatroid@gmail.com>
2024-12-24 02:07:02 +0900

    feat: echo server

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,6 @@
+use rocket::futures::{SinkExt, StreamExt};
+use rocket_ws::{Channel, WebSocket};
+
 #[rocket::main]
 async fn main() {
     let _ = rocket::build()
@@ -7,4 +10,13 @@ async fn main() {
 }
 
 #[rocket::get("/")]
-fn chat() {}
+fn chat(ws: WebSocket) -> Channel<'static> {
+    ws.channel(move |mut stream| {
+        Box::pin(async move {
+            while let Some(message) = stream.next().await {
+                let _ = stream.send(message?).await;
+            }
+            Ok(())
+        })
+    })
+}
